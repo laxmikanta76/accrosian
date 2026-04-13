eb4bb5c01f3f75932de7ac53008c21bf6045e17f
Laxmikanta <laxmikanta.accrosian@gmail.com>
2026-04-13 12:24:32 +0530

    adding airs

--- resources/views/frontend/airs.blade.php
+++ resources/views/frontend/airs.blade.php
@@ -459,7 +459,7 @@
 </section>
 
 {{-- ═══════════ HOW AIRS OPERATES ═══════════ --}}
-<section style="padding:90px 0;background:var(--navy-mid)">
+<section class="cta-section style=" padding:90px 0;background:var(--navy-mid)">
     <div class="container">
         <div style="text-align:center;margin-bottom:60px" class="reveal">
             <span class="section-tag">How It Works</span>
