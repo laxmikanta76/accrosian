d8e0fd0342c373ec3dd6e58d56e507dfd823ff09
Laxmikanta <laxmikanta.accrosian@gmail.com>
2026-04-14 15:32:39 +0530

    changing in hero section

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -8,7 +8,7 @@ delivering innovative web, mobile, cloud, and AI solutions for modern businesses
 
 {{-- HERO --}}
 <section class="hero">
-    <!-- <img src="{{ asset('assets/images/hero-img.png') }}" alt="Hero Background" class="hero-bg-img" /> -->
+    <img src="{{ asset('assets/images/for-bg-img.jpeg') }}" alt="Hero Background" class="hero-bg-img" />
     <div class="hero-bg-effects">
         <div class="hero-orb hero-orb-1"></div>
         <div class="hero-orb hero-orb-2"></div>
